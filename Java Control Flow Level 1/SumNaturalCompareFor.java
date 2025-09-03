be3263c8ce011e15f41f96873c538875bdde57a0
Shivam Shukla <shuklashivam63063@gmail.com>
2025-09-03 10:56:27 +0530

    Second

---
+++ Java Control Flow Level 1/SumNaturalCompareFor.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SumNaturalCompareFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n > 0) {
+            int sum = 0;
+            for (int i = 1; i <= n; i++) {
+                sum += i;
+            }
+            int formula = n * (n + 1) / 2;
+            System.out.println("Sum using loop: " + sum);
+            System.out.println("Sum using formula: " + formula);
+        } else {
+            System.out.println("Not a natural number");
+        }
+    }
+}
